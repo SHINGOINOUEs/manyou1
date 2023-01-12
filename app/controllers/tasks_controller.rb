6ae9f987305411f09414e5e0a71170859b987c76
shingo inoue <inoueshingo@INOUESHINGOnoMacBook-Pro.local>
2023-01-12 15:33:33 +0900

    “一覧詳細画面”

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,20 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all    
   end
+
+  def create
+    Task.create(task_params)
+    redirect_to new_task_path
+  end
+
+  def show
+    @task= Task.find(params[:id])
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end  
 end
