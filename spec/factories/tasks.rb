da2a0b226ae0ef39d9f01edb272788c36a625679
shingo inoue <inoueshingo@INOUESHINGOnoMacBook-Pro.local>
2023-01-15 11:53:25 +0900

    “システムスペック”

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :task do
     # 下記の内容は実際に作成するカラム名に合わせて変更してください
-    title { 'test_title' }
-    content { 'test_content' }
+    title { 'Factoryで作ったデフォルトのタイトル１' }
+    content { 'Factoryで作ったデフォルトのコンテント１' }
   end
 end
