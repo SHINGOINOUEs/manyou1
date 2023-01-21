ed75d45144499c14466a2a9130ef35ce58602935
shingo inoue <inoueshingo@INOUESHINGOnoMacBook-Pro.local>
2023-01-21 09:19:05 +0900

    “終了期限テーブルに追加”

---
+++ db/migrate/20230121001605_add_title_to_tasks.rb
@@ -0,0 +1,5 @@
+class AddTitleToTasks < ActiveRecord::Migration[6.0]
+  def change
+    add_column :tasks, :deadline, :datetime
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_12_231310) do
+ActiveRecord::Schema.define(version: 2023_01_21_001605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2023_01_12_231310) do
     t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "deadline"
   end
 
 end
