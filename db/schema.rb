980ed0dd94a4361176109cbbc74a8c12135687a5
shingo inoue <zhangmoutou@yahoo.co.jp>
2023-01-30 19:10:55 +0900

    “優先順位データベース”

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_25_005833) do
+ActiveRecord::Schema.define(version: 2023_01_30_095540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2023_01_25_005833) do
     t.datetime "updated_at", precision: 6, null: false
     t.date "deadline", null: false
     t.integer "status"
+    t.integer "priority"
   end
 
 end
